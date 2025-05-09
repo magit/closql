c90bb6536307d26943442a69137d6f03675aa2d7
Jonas Bernoulli <jonas@bernoul.li>
2025-05-09 16:23:04 +0200

    Update .dir-locals.el

--- .dir-locals.el
+++ .dir-locals.el
@@ -3,4 +3,8 @@
  (makefile-mode
   (indent-tabs-mode . t))
  (git-commit-mode
-  (git-commit-major-mode . git-commit-elisp-text-mode)))
+  (git-commit-major-mode . git-commit-elisp-text-mode))
+ ("CHANGELOG"
+  (nil
+   (fill-column . 70)
+   (mode . display-fill-column-indicator))))
